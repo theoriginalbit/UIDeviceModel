29c153d665263034e7907674aaefc72581d2ce65
theoriginalbit <theoriginalbit@gmail.com>
2020-09-24 00:28:13 +1000

    Add iPad 8th Generation and iPad Air 4th Generation

--- Sources/UIDeviceModel/UIDeviceModel.swift
+++ Sources/UIDeviceModel/UIDeviceModel.swift
@@ -10,26 +10,29 @@ import Foundation
 
 public enum UIDeviceModel {
     case iPad2
-    case iPad3
-    case iPad4
-    case iPad5
-    case iPad6
+    case iPadGen3
+    case iPadGen4
+    case iPadGen5
+    case iPadGen6
+    case iPadGen7
+    case iPadGen8
     case iPadAir
     case iPadAir2
-    case iPadAir3
-    case iPadPro9Inch
-    case iPadPro10Inch
+    case iPadAirGen3
+    case iPadAirGen4
+    case iPadPro9_7Inch
+    case iPadPro10_5Inch
     case iPadPro11Inch
-    case iPadPro11Inch2
-    case iPadPro12Inch
-    case iPadPro12Inch2
-    case iPadPro12Inch3
-    case iPadPro12Inch4
+    case iPadPro11InchGen2
+    case iPadPro12_9Inch
+    case iPadPro12_9InchGen2
+    case iPadPro12_9InchGen3
+    case iPadPro12_9InchGen4
     case iPadMini
     case iPadMini2
     case iPadMini3
     case iPadMini4
-    case iPadMini5
+    case iPadMiniGen5
 
     case iPhone4s
     case iPhone5
@@ -51,7 +54,7 @@ public enum UIDeviceModel {
     case iPhone11
     case iPhone11Pro
     case iPhone11ProMax
-    case iPhoneSE2
+    case iPhoneSEGen2
 
     case iPodTouch5
     case iPodTouch6
@@ -98,25 +101,28 @@ extension UIDeviceModel {
         switch identifier {
         case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4": return .iPad2
         case "iPad2,5", "iPad2,6", "iPad2,7": return .iPadMini
-        case "iPad3,1", "iPad3,2", "iPad3,3": return .iPad3
-        case "iPad3,4", "iPad3,5", "iPad3,6": return .iPad4
+        case "iPad3,1", "iPad3,2", "iPad3,3": return .iPadGen3
+        case "iPad3,4", "iPad3,5", "iPad3,6": return .iPadGen4
         case "iPad4,1", "iPad4,2", "iPad4,3": return .iPadAir
         case "iPad4,4", "iPad4,5", "iPad4,6": return .iPadMini2
         case "iPad4,7", "iPad4,8", "iPad4,9": return .iPadMini3
         case "iPad5,1", "iPad5,2": return .iPadMini4
         case "iPad5,3", "iPad5,4": return .iPadAir2
-        case "iPad6,3", "iPad6,4": return .iPadPro9Inch
-        case "iPad6,7", "iPad6,8": return .iPadPro12Inch
-        case "iPad6,11", "iPad6,12": return .iPad5
-        case "iPad7,1", "iPad7,2": return .iPadPro12Inch2
-        case "iPad7,3", "iPad7,4": return .iPadPro10Inch
-        case "iPad7,5", "iPad7,6": return .iPad6
+        case "iPad6,3", "iPad6,4": return .iPadPro9_7Inch
+        case "iPad6,7", "iPad6,8": return .iPadPro12_9Inch
+        case "iPad6,11", "iPad6,12": return .iPadGen5
+        case "iPad7,1", "iPad7,2": return .iPadPro12_9InchGen2
+        case "iPad7,3", "iPad7,4": return .iPadPro10_5Inch
+        case "iPad7,5", "iPad7,6": return .iPadGen6
+        case "iPad7,11", "iPad7,12": return .iPadGen7
         case "iPad8,1", "iPad8,2", "iPad8,3", "iPad8,4": return .iPadPro11Inch
-        case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8": return .iPadPro12Inch3
-        case "iPad8,9", "iPad8,10": return .iPadPro11Inch2
-        case "iPad8,11", "iPad8,12": return .iPadPro12Inch4
-        case "iPad11,1", "iPad11,2": return .iPadMini5
-        case "iPad11,3", "iPad11,4": return .iPadAir3
+        case "iPad8,5", "iPad8,6", "iPad8,7", "iPad8,8": return .iPadPro12_9InchGen3
+        case "iPad8,9", "iPad8,10": return .iPadPro11InchGen2
+        case "iPad8,11", "iPad8,12": return .iPadPro12_9InchGen4
+        case "iPad11,1", "iPad11,2": return .iPadMiniGen5
+        case "iPad11,3", "iPad11,4": return .iPadAirGen3
+        case "iPad11,6", "iPad11,7": return .iPadGen8
+        case "iPad13,1", "iPad13,2": return .iPadAirGen4
 
         case "iPhone4,1": return .iPhone4s
         case "iPhone5,1", "iPhone5,2": return .iPhone5
@@ -138,7 +144,7 @@ extension UIDeviceModel {
         case "iPhone12,1": return .iPhone11
         case "iPhone12,3": return .iPhone11Pro
         case "iPhone12,5": return .iPhone11ProMax
-        case "iPhone12,8": return .iPhoneSE2
+        case "iPhone12,8": return .iPhoneSEGen2
 
         case "iPod5,1": return .iPodTouch5
         case "iPod7,1": return .iPodTouch6
